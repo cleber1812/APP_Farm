cf0b7ed33e71fca47a2aa6a3c8400a37150606ae
Cleber1812 <cleber1812roberto@gmail.com>
2024-12-28 12:28:04 -0300

    Removendo React do import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -57,7 +57,7 @@ export function Home () {
       setCalculatedPlants(Math.floor(area / (rowSpacing * plantSpacing)));
       setPlants(calculatedPlants)
     }
-    
+
 
     // Cycle start dates calculations
     const secondStart = firstCycleStart + firstCycleDuration + 60;
